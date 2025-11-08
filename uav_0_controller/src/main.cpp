1b348bb3db0b3a8ef3e38ae68150e2a47e67db55
kartik <kartik8virmani@gmail.com>
2025-11-07 23:58:20 -0500

    s3 wired

--- uav_0_controller/src/main.cpp
+++ uav_0_controller/src/main.cpp
@@ -1,18 +1,29 @@
 #include <Arduino.h>
+#include <Adafruit_NeoPixel.h>
+#ifndef RGB_PIN
+#define RGB_PIN  21  
+#endif
 
-// put function declarations here:
-int myFunction(int, int);
+#define NUM_PIXELS 1
+
+Adafruit_NeoPixel led(NUM_PIXELS, RGB_PIN, NEO_GRB + NEO_KHZ800);
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  led.begin();
+  led.clear();
+  led.setBrightness(32);     
+  led.setPixelColor(0, led.Color(255, 0, 0));
+  led.show();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  // simple blink
+  static uint32_t t0 = millis();
+  if (millis() - t0 > 500) {
+    t0 = millis();
+    static bool on = false;
+    on = !on;
+    led.setPixelColor(0, on ? led.Color(0, 255, 0) : 0); 
+    led.show();
+  }
 }
